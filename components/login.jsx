cb539b94c5cc666d82543ded00f245f8c1aa6eca
Leon Erasmus <leondevcode@gmail.com>
2022-04-09 17:32:38 -0400

    login: frontend validation and error messages

--- components/login.jsx
+++ components/login.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form'; //validation
 
 export default function Login() {
-  function handleSubmit() {
-    console.log('handleSubmit');
-  }
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch('email'));
+  console.log(watch('password'));
+  console.log(errors);
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h3 className="my-3">Ingresa con tu cuenta</h3>
         <div className="mb-3">
           <input
@@ -16,7 +26,20 @@ export default function Login() {
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
             placeholder="correo"
+            {...register('email', {
+              required: true,
+              pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+            })}
           />
+          {errors.email ? (
+            errors.email.type === 'pattern' ? (
+              <p className="error-message">Correo no es valido</p>
+            ) : (
+              <p className="error-message">Correo requerido </p>
+            )
+          ) : (
+            ''
+          )}
         </div>
         <div className="mb-3">
           <input
@@ -24,15 +47,29 @@ export default function Login() {
             className="form-control"
             id="exampleInputPassword1"
             placeholder="Contraseña"
+            {...register('password', { required: true, minLength: 8 })}
           />
+
+          {errors.password ? (
+            errors.password.type === 'minLength' ? (
+              <p className="error-message">Contraseña minimo 8 caracteres</p>
+            ) : (
+              <p className="error-message">Contraseña requerida </p>
+            )
+          ) : (
+            ''
+          )}
         </div>
 
         <div className="d-grid gap-2 col-12 mx-auto">
-          <Link href="/user-info" passHref>
+          <button className="btn loginBtn" type="submit">
+            Ingresar
+          </button>
+          {/*    <Link href="/user-info" passHref>
             <button className="btn loginBtn" type="submit">
               Ingresar
             </button>
-          </Link>
+          </Link> */}
         </div>
         <p className="my-4 text-center">Olvidé mi contraseña</p>
       </form>
