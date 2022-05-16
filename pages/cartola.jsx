d0448b5e5934fc479d39d5f604ec5526bb62b0ec
Leon Erasmus <leondevcode@gmail.com>
2022-05-16 16:56:43 -0400

    login/logout/changepassword

--- pages/cartola.jsx
+++ pages/cartola.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Store } from "../utils/store"; //store
 import { useRouter } from 'next/router';
 import Link from "next/link";
